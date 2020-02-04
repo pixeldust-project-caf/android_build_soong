68dcd4e57c0aa66d8ba3a623606387fbd63b4bf4
Surge1223 <Surge1223@gmail.com>
2020-02-04 07:25:20 +0000

    Re-allow relative paths for modules
    Change-Id: Ib76b78faa3ef05d02da01d820ece7fa18194ba86
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- android/paths.go
+++ android/paths.go
@@ -362,10 +362,6 @@ func pathsForModuleSrcFromFullPath(ctx ModuleContext, paths []string, incDirs bo
 			continue
 		}
 		path := filepath.Clean(p)
-		if !strings.HasPrefix(path, prefix) {
-			reportPathErrorf(ctx, "Path %q is not in module source directory %q", p, prefix)
-			continue
-		}
 
 		srcPath, err := safePathForSource(ctx, ctx.ModuleDir(), path[len(prefix):])
 		if err != nil {
@@ -1292,7 +1288,7 @@ func modulePartition(ctx ModuleInstallPathContext) string {
 func validateSafePath(pathComponents ...string) (string, error) {
 	for _, path := range pathComponents {
 		path := filepath.Clean(path)
-		if path == ".." || strings.HasPrefix(path, "../") || strings.HasPrefix(path, "/") {
+		if strings.HasPrefix(path, "/") {
 			return "", fmt.Errorf("Path is outside directory: %s", path)
 		}
 	}
@@ -1425,7 +1421,7 @@ func MaybeRel(ctx PathContext, basePath string, targetPath string) (string, bool
 	if err != nil {
 		reportPathError(ctx, err)
 		return "", false
-	} else if rel == ".." || strings.HasPrefix(rel, "../") || strings.HasPrefix(rel, "/") {
+	} else if strings.HasPrefix(rel, "/") {
 		return "", false
 	}
 	return rel, true
