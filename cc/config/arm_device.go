d65f0c33dce586421e1633fa8bb5b1c79e99a932
spezi77 <spezi7713@gmx.net>
2021-12-16 10:43:08 +0100

    Revert "Remove workaround for Qualcomm Kryo 385"
    This reverts commit 6603cce710cd9e596e240e1b6276b7e93574d5af.
    
    This wasn't a good idea without having the specific clang toolchain handy!
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- cc/config/arm_device.go
+++ cc/config/arm_device.go
@@ -151,9 +151,8 @@ var (
 			"-D__ARM_FEATURE_LPAE=1",
 		},
 		"kryo385": []string{
-			// Use cortex-a55 because kryo385 is not supported in GCC/clang.
-			"-mcpu=cortex-a55",
-			"-mfpu=neon-fp-armv8",
+			// Use cortex-a53 because kryo385 is not supported in GCC/clang.
+			"-mcpu=cortex-a53",
 			// Fake an ARM compiler flag as these processors support LPAE which GCC/clang
 			// don't advertise.
 			// TODO This is a hack and we need to add it for each processor that supports LPAE until some
@@ -214,8 +213,6 @@ func init() {
 		strings.Join(armClangCpuVariantCflags["krait"], " "))
 	pctx.StaticVariable("ArmClangKryoCflags",
 		strings.Join(armClangCpuVariantCflags["kryo"], " "))
-	pctx.StaticVariable("ArmClangKryo385Cflags",
-		strings.Join(armClangCpuVariantCflags["kryo385"], " "))
 }
 
 var (
@@ -240,7 +237,7 @@ var (
 		"cortex-a76":     "${config.ArmClangCortexA55Cflags}",
 		"krait":          "${config.ArmClangKraitCflags}",
 		"kryo":           "${config.ArmClangKryoCflags}",
-		"kryo385":        "${config.ArmClangKryo385Cflags}",
+		"kryo385":        "${config.ArmClangCortexA53Cflags}",
 		"exynos-m1":      "${config.ArmClangCortexA53Cflags}",
 		"exynos-m2":      "${config.ArmClangCortexA53Cflags}",
 	}
